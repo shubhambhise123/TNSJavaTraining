72463327421026d4f148cc7ea3ecfd654061fffc
shubhambhise123 <157010778+shubhambhise123@users.noreply.github.com>
2024-04-03 11:47:40 +0530

    Add files via upload
    Program for ASCII value

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+/**
+ * Ascii
+ */
+public class Ascii {
+
+    public static void main(String[] args) {
+        char ch='A';
+        int i=ch;
+        System.out.println("ASCII value for A");
+        System.out.println(i);
+    }
+}
